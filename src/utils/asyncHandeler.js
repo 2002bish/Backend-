940f8622bd0a50cc8f2b1dfd824bf24228531c01
2002bish <bniroula707@gmail.com>
2024-09-26 17:02:45 +0545

    utils file added

--- src/utils/asyncHandeler.js
+++ src/utils/asyncHandeler.js
@@ -1,3 +1,21 @@
-const asyncHandeler = () => {}
+const asyncHandeler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
 
 export {asyncHandeler}
+
+
+//This second step is also done for above process
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
