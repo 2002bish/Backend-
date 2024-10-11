98159eae3cc4c12783ab28db8e75901eebb0fdeb
2002bish <bniroula707@gmail.com>
2024-10-11 07:42:45 +0545

    user.controller file is updated

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,29 @@
 import {asyncHandler} from "../utils/asyncHandeler.js";
 
 const registerUser= asyncHandler (async (req, res) =>{
-    res.status(200).json({
-        message:"ok"
+    const {fullName,email,username,password} = req.body
+    if (
+        [fullName,email,username,password].some ((field)
+    =>field?.trim()=="")
+    ) {
+        throw new ApiError(400, "All fields are required")
+    }
+    const existedUser =await UserActivation.findone ({
+        $or: [{username}, {email}]
     })
+    if (existedUser){
+        throw new ApiError (409, "User with email or username already exist")
+    }
+    const avatarLocalpath= req.files?.avatar[0]?.path;
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length >0){
+        coverImageLocalPath= req.files.coverImage[0].path
+    }
+
+    if (!avatarLocalpath){
+        throw new ApiError (400, "Avatar file is required")
+    }
 })
 
 
