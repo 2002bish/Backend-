3cf2393a6c56652a964dc44ec4eb80531b031363
2002bish <bniroula707@gmail.com>
2024-10-23 19:20:12 +0545

    video.route added

---
+++ src/routes/video.route.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import {
+    deleteVideo,
+    getAllVideos,
+    getVideoById,
+    publishVideo,
+    togglePublishStatus,
+    updateVideo,
+} from "../controllers/video.controller.js"
